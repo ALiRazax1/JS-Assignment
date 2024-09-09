4e6bcf3bb3cb6a1b772622880a53a9712d40eddd
Ali Raza <161970819+ALiRazax1@users.noreply.github.com>
2024-09-10 00:15:24 +0500

    Upload 3chapters

---
+++ Chapter-01/main.js
@@ -0,0 +1,12 @@
+//01. Write a script to greet your website visitor using JS alert box.
+alert('Welcome to Our Site')
+// 02. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password")
+// 03. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land\n Happy Coding!")
+// 04. Write a script to display following messages in sequence:
+// Welcome to JS Land
+// Happy Coding
+alert('Welcome to JS Land')
+alert('Happy Coding\n')
+
---
+++ Chapter-03/main.js
@@ -0,0 +1,38 @@
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+var age = 25
+alert("I am " + age + " years old")
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+var numberOfTimesVisited= 10
+alert('You have visitef this site ' + numberOfTimesVisited+' times')
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var  birthYear = 1997
+document.write('My birth year is ' + birthYear+ '<br>' + "Data type of my vatiable is number" + '<br>')
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+var visitorName = 'Mukarram'
+var productTitile = 'T-shirt'
+var quantity = 5
+
+document.write(visitorName + ' ordered ' + quantity + ' ' + productTitile + ' on XYZ Clothing store'+ '<br>')
+
